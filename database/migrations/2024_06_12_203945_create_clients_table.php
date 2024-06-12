e7fdf9966d72eca485b61ae72d959ea6c44bdbe7
Manuel Echavarria <mechavarria@aldereca.com>
2024-06-12 17:38:56 -0400

    avances db

---
+++ database/migrations/2024_06_12_203945_create_clients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('clients', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('created_by_user_id')->constrained('users');
+            $table->string('rnc', 11)->nullable();
+            $table->string('business_name', 150)->nullable();
+            $table->string('commercial_activity')->nullable();
+            $table->string('email')->nullable();
+            $table->string('phone', 11)->nullable();
+            $table->boolean('is_active')->default(true);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('clients');
+    }
+};
---
+++ database/migrations/2024_06_12_203948_create_invoices_table.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('client_id')->constrained();
+            $table->string('rnc', 11)->nullable();
+
+            $this->addUnsignedNullableInteger($table, 'identification_type');
+
+            $table->string('ncf', 19)->nullable();
+            $table->string('ncf_modified', 19)->nullable();
+
+            $this->addUnsignedNullableInteger($table, 'income_type');
+
+            $table->string('proof_date', 8)->nullable();
+            $table->string('withholding_date', 8)->nullable();
+
+            $this->addUnsignedNullableInteger($table, 'amount');
+            $this->addUnsignedNullableInteger($table, 'itbis');
+            $this->addUnsignedNullableInteger($table, 'third_party_itbis');
+            $this->addUnsignedNullableInteger($table, 'withheld_itbis');
+            $this->addUnsignedNullableInteger($table, 'third_party_income_retention');
+            $this->addUnsignedNullableInteger($table, 'isr');
+            $this->addUnsignedNullableInteger($table, 'selective_tax');
+            $this->addUnsignedNullableInteger($table, 'other_taxes_fees');
+            $this->addUnsignedNullableInteger($table, 'legal_tip_amount');
+            $this->addUnsignedNullableInteger($table, 'cash');
+            $this->addUnsignedNullableInteger($table, 'check_transfer_deposit');
+            $this->addUnsignedNullableInteger($table, 'debit_credit_card');
+            $this->addUnsignedNullableInteger($table, 'credit_sale');
+            $this->addUnsignedNullableInteger($table, 'bonds_certificates');
+            $this->addUnsignedNullableInteger($table, 'barter');
+            $this->addUnsignedNullableInteger($table, 'other_sales_forms');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Add an unsigned nullable integer column to the table
+     */
+    private function addUnsignedNullableInteger(Blueprint $table, string $columnName): void
+    {
+        $table->integer($columnName)->unsigned()->default(0)->nullable();
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
