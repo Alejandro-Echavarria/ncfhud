a14e20c0a0282a85d6ff8628c143b56b03992052
Manuel Alejandro Echavarría Taveras <manuelalejandroechavarriataveras@Manuels-MacBook-Air.local>
2024-07-10 22:25:40 -0400

    avances

--- resources/js/Pages/Admin/Invoices/Index.vue
+++ resources/js/Pages/Admin/Invoices/Index.vue
@@ -7,6 +7,7 @@ import MainTable from '@/Components/Main/Admin/Components/Tables/MainTable.vue';
 import SaveClient from "@/Pages/Admin/Clients/Partials/SaveClient.vue";
 import Search from '@/Components/Main/Admin/Components/Searchs/Search.vue';
 import VueSelect from "@/Components/Main/Admin/Components/Selects/VueSelect.vue";
+import InputLabel from "@/Components/InputLabel.vue";
 
 defineOptions({
     layout: MainLayout
@@ -37,14 +38,17 @@ const openModal = (op, id, rnc, business_name, commercial_activity, email) => {
 
     <div class="grid grid-cols-1 sm:grid-cols-4 gap-6">
         <div class="sm:col-span-2">
+            <InputLabel for="client" value="Cliente" />
             <VueSelect />
         </div>
 
         <div>
+            <InputLabel for="month" value="Mes" />
             <VueSelect />
         </div>
 
         <div>
+            <InputLabel for="year" value="Año" />
             <VueSelect />
         </div>
     </div>
