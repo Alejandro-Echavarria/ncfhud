e8ec820cb9622b613e142dc95066dbc4f353dff6
Manuel Alejandro Echavarria Taveras <mechavarria@alpha.com.do>
2024-10-28 20:29:37 -0400

    correccion icono y logo

--- resources/js/Components/AuthenticationCardLogo.vue
+++ resources/js/Components/AuthenticationCardLogo.vue
@@ -5,6 +5,6 @@ import ApplicationLogo from '@/Components/ApplicationLogo.vue';
 
 <template>
     <Link :href="'/'">
-        <ApplicationLogo :class="'h-14 h-14'" :entity-name="$page.props.entity?.name" :url="$page.props.entity?.url" />
+        <ApplicationLogo :class="'w-16 h-16'" :entity-name="$page.props.entity?.name" :url="$page.props.entity?.url" />
     </Link>
 </template>
