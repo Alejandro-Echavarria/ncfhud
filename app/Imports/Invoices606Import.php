4c234daee83c802a6d9ecc66b5511cae2c7b0dae
Manuel Alejandro Echavarría Taveras <manuelalejandroechavarriataveras@Manuels-MacBook-Air.local>
2024-12-24 00:13:03 -0400

    avances con campos fechas

--- app/Imports/Invoices606Import.php
+++ app/Imports/Invoices606Import.php
@@ -3,11 +3,13 @@
 namespace App\Imports;
 
 use App\Models\Invoice606;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Maatwebsite\Excel\Concerns\ToModel;
 use Maatwebsite\Excel\Concerns\WithCalculatedFormulas;
 use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use Maatwebsite\Excel\Concerns\WithValidation;
+use PhpOffice\PhpSpreadsheet\Shared\Date;
 
 class Invoices606Import implements ToModel, WithValidation, WithHeadingRow, WithCalculatedFormulas
 {
@@ -36,7 +38,8 @@ class Invoices606Import implements ToModel, WithValidation, WithHeadingRow, With
             'rnc' => $row['rnc_cedula'],
             'business_name' => $row['razon_social'],
             'ncf' => $row['numero_de_comprobante'],
-            'proof_date' => $row['fecha_comprobante'],
+            'proof_date' => is_string($row['fecha_comprobante']) ? Carbon::parse($row['fecha_comprobante'])->format('Ymd') :  date('Ymd', ($row['fecha_comprobante'] - 25569) * 86400),
+//            'proof_date' => is_string($row['fecha_comprobante']) ? $row['fecha_comprobante'] : DATE::excelToDateTimeObject($row['fecha_comprobante'])->format('Ymd'),
             'payment_date' => $getOrZero($row['fecha_de_pago']),
             'amount' => $getOrZero($row['monto_facturado']),
             'itbis' => $getOrZero($row['itbis_facturado']),
@@ -49,9 +52,9 @@ class Invoices606Import implements ToModel, WithValidation, WithHeadingRow, With
         return [
             'fecha_comprobante' => [
                 'required',
-                'regex:/^\d{4}(0[1-9]|1[0-2])([0-2][0-9]|3[0-1])$/'
+//                'regex:/^\d{4}(0[1-9]|1[0-2])([0-2][0-9]|3[0-1])$/'
             ],
-            'numero_de_comprobante' => 'required|string|max:19|unique:invoices_606,ncf',
+//            'numero_de_comprobante' => 'required|string|max:19|unique:invoices_606,ncf',
         ];
     }
 
